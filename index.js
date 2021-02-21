90940c291a7b0e9203b5e41aee1e6c40b36e6d50
Nacho Soloaga <nachosoloaga@gmail.com>
2021-02-21 03:00:00 -0300

    Added CLI as entry-point of application

---
+++ index.js
@@ -0,0 +1,55 @@
+const puppeteer = require('puppeteer');
+const reader = require('./lib/reader');
+const worker = require('./lib/worker');
+
+const cli = {};
+
+cli.processWebsites = async () => {
+  const websites = await reader.readCsv();
+  const browser = await puppeteer.launch({ headless: true });
+
+  const result = await Promise.all(websites.map((site) => worker.processWebsite(site, browser)));
+
+  await browser.close();
+
+  return result;
+};
+
+cli.dependencies = (results) => {
+  results.map((site) => {
+    site.dependencies.map((d) => console.log(`${site.title} , ${d}`));
+    return null;
+  });
+};
+
+cli.contentLength = (results) => {
+  results.map((site) => {
+    console.log(`${site.title}, ${site.content_length}`);
+    return null;
+  });
+};
+
+cli.start = async () => {
+  if (!process.argv[2]) {
+    console.log(
+      'Please indicate a task (--dependencies, --contentLength, --totalDependencies, --all)'
+    );
+
+    return;
+  }
+
+  const results = await cli.processWebsites();
+
+  switch (process.argv[2]) {
+    case '--dependencies':
+      cli.dependencies(results);
+      break;
+    case '--contentLength':
+      cli.contentLength(results);
+      break;
+    default:
+      console.log('Unknown parameter');
+  }
+};
+
+cli.start();
